e245ac5dbd33297c4bb73161765528e226f90698
mhtbansal11 <mhtbansal11@gmail.com>
2022-08-25 18:58:52 +0530

    world&news

---
+++ scripts/news.js
@@ -0,0 +1,65 @@
+let newsData= JSON.parse(localStorage.getItem("data")) || [];
+console.log(newsData);
+let worldNewsDiv = document.getElementById("world_news");
+  worldNewsDiv.innerHTML = null;
+//   data.forEach((element) => {
+    let div = document.createElement("div");
+    // let box = document.createElement("div");
+    let title = document.createElement("h2");
+    title.innerText = newsData.title;
+    let desc = document.createElement("p");
+    desc.innerText = newsData.description;
+    let published = document.createElement("p");
+    published.innerText = `${newsData.source.name} | ${newsData.publishedAt}`;
+    let img = document.createElement("img");
+    img.src = newsData.urlToImage;
+    // box.append(title, published, desc);
+    div.append(img,title, published, desc);
+    worldNewsDiv.append(div);
+     
+//   };
+
+let trending;
+let worldNews;
+async function myTrending() {
+  // in try put whatever we want to work
+  try {
+    let stream = await fetch(
+      "https://newsapi.org/v2/top-headlines?language=en&sortBy=popularity&apiKey=01e4b4bb46844c7f85fac3ee76078269"
+    );
+    // convert the stream into actual data
+    let data = await stream.json();
+    trending = data.articles;
+    console.log("trending:", trending);
+    trendingAppend(trending);
+  } catch (err) {
+    // in catch put the error part
+    console.log("err", err);
+  }
+}
+myTrending();
+function trendingAppend(data) {
+    let trendingDiv = document.getElementById("trending");
+    //   trendingDiv.innerHTML = null;
+  
+    data.forEach((element) => {
+      let div = document.createElement("div");
+      
+  
+      let title = document.createElement("h5");
+      title.innerText = element.title;
+  
+      // if (element.urlToImage === null) {
+      //     let img = document.createElement("img");
+      //     img.src ="https://images.hindustantimes.com/rf/image_size_640x362/HT/p2/2016/11/05/Pictures/_8ba2e79a-a350-11e6-93ed-ab826829dd0b.png"
+      //     div.append(img,title);
+      //     trendingDiv.append(div);
+      //   } else {
+      let img = document.createElement("img");
+      img.src = element.urlToImage;
+      div.append(img, title);
+  
+      //   }
+      trendingDiv.append(div);
+    });
+  }
---
+++ scripts/world.js
@@ -0,0 +1,119 @@
+let trending;
+let worldNews;
+async function myTrending() {
+  // in try put whatever we want to work
+  try {
+    let stream = await fetch(
+      "https://newsapi.org/v2/top-headlines?language=en&sortBy=popularity&apiKey=01e4b4bb46844c7f85fac3ee76078269"
+    );
+    // convert the stream into actual data
+    let data = await stream.json();
+    trending = data.articles;
+    console.log("trending:", trending);
+    trendingAppend(trending);
+  } catch (err) {
+    // in catch put the error part
+    console.log("err", err);
+  }
+}
+myTrending();
+
+async function myWorldNews() {
+  try {
+    let stream = await fetch(
+      "https://newsapi.org/v2/top-headlines?language=en&apiKey=01e4b4bb46844c7f85fac3ee76078269"
+    );
+    // convert the stream into actual data
+    let data = await stream.json();
+    worldNews = data.articles;
+    console.log("worldNews:", worldNews);
+    worldNewsAppend(worldNews);
+  } catch (err) {
+    // in catch put the error part
+    console.log("err", err);
+  }
+}
+myWorldNews();
+
+async function worldNewsCountry() {
+  try {
+    let country = document.getElementById("country").value;
+    console.log("country:",country)
+    let stream = await fetch(
+      `https://newsapi.org/v2/top-headlines?language=en&country=${country}&apiKey=01e4b4bb46844c7f85fac3ee76078269`
+    );
+    // convert the stream into actual data
+    let data = await stream.json();
+    worldNewsCountry = data.articles;
+    console.log("worldNewsCountry:", worldNewsCountry);
+    worldNewsAppend(worldNewsCountry);
+  } catch (err) {
+    // in catch put the error part
+    console.log("err", err);
+  }
+}
+
+function trendingAppend(data) {
+  let trendingDiv = document.getElementById("trending");
+  //   trendingDiv.innerHTML = null;
+
+  data.forEach((element) => {
+    let div = document.createElement("div");
+    div.addEventListener("click", function () {
+      NewsData(element);
+    });
+
+    let title = document.createElement("h5");
+    title.innerText = element.title;
+
+    // if (element.urlToImage === null) {
+    //     let img = document.createElement("img");
+    //     img.src ="https://images.hindustantimes.com/rf/image_size_640x362/HT/p2/2016/11/05/Pictures/_8ba2e79a-a350-11e6-93ed-ab826829dd0b.png"
+    //     div.append(img,title);
+    //     trendingDiv.append(div);
+    //   } else {
+    let img = document.createElement("img");
+    img.src = element.urlToImage;
+    div.append(img, title);
+
+    //   }
+    trendingDiv.append(div);
+  });
+}
+
+function worldNewsAppend(data) {
+  let worldNewsDiv = document.getElementById("world_news");
+  worldNewsDiv.innerHTML = null;
+  data.forEach((element) => {
+    let div = document.createElement("div");
+    div.addEventListener("click", function () {
+      NewsData(element);
+    });
+    let box = document.createElement("div");
+    let title = document.createElement("h2");
+    title.innerText = element.title;
+    let desc = document.createElement("p");
+    desc.innerText = element.description;
+    let published = document.createElement("p");
+    published.innerText = `${element.source.name} | ${element.publishedAt}`;
+    published.className = "publish"
+    // if (element.urlToImage === null) {
+    //   let img = document.createElement("img");
+    //   img.src ="https://images.hindustantimes.com/rf/image_size_640x362/HT/p2/2016/11/05/Pictures/_8ba2e79a-a350-11e6-93ed-ab826829dd0b.png"
+    //   div.append(img,title, published,desc);
+    //   worldNewsDiv.append(div);
+    // } else {
+    let img = document.createElement("img");
+    img.src = element.urlToImage;
+    box.append(title, published, desc);
+    div.append(img, box);
+    worldNewsDiv.append(div);
+    // }
+  });
+}
+
+let NewsData = (element) => {
+  console.log(element);
+  localStorage.setItem("data", JSON.stringify(element));
+  window.location.href = "./news.html";
+};
