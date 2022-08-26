c94f9b533d39cc7540e614b9de4d005eb5909ce7
Brar21 <103635395+Brar21@users.noreply.github.com>
2022-08-27 00:27:48 +0530

    day5

--- script/india.js
+++ script/india.js
@@ -3,75 +3,80 @@ document.getElementById("navbar").innerHTML = navbar();
 // let url = `http://api.mediastack.com/v1/news?access_key=70e250225f82fd05fd9a1daf86aa4a2a&languages=en&countries=in&limit=100`;
 let url = `http://api.mediastack.com/v1/news?access_key=ce7ea8c6513f70ccecd1e152e928ac83&languages=en&countries=in&limit=100`;
 
-
-// let url = `https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=cac2a3600da541eebf8893025a6f052a`;
-
-let DaTa = [];
-let pagesize = 5;
-let currentPage = 1;
-
-// let container = document.getElementById('center');
-
-// container.innerHTML=null
-async function render(page=1) {
-    await get()
-    var contain = ""
-
-    if (page === 1) {
-        prevButton.style.visibility="hidden"
-    } else {
-        prevButton.style.visibility = "visible"
-    }
-
-    if (page === numberOfpage()) {
-        nextButton.style.visibility = "hidden"
-    } else {
-        nextButton.style.visibility = "visible"
-    }
-    console.log(DaTa)
-    DaTa.filter((row, index) => {
-        let start = (currentPage - 1) * pagesize
-        let end = currentPage * pagesize
-        
-        if (index >= start && index < end) return true;
-    }).forEach(el => {
-        contain+=`<img id="on" src=${el.image}>`
-        contain += `<h3 id="on">${(el.title)}</h3>`
-        contain += `<h6 id="on">${(el.author)} | ${(el.publishedAt)}</h6>`
-        contain += `<p id="on">${(el.description)}</p>`
-    })
-    document.getElementById('center').innerHTML=contain;
-// console.log(contain)
-}
-
-render()
-
-function prePage() {
-    if (currentPage > 1)
-        currentPage--;
-    render(currentPage)
-}
-function nextPage() {
-    if (currentPage * pagesize < DaTa.length)
-        currentPage++;
-    console.log(currentPage)
-    render(currentPage)
-}
-
-function numberOfpage() {
-    return Math.ceil(DaTa.length / pagesize)
-    
-}
-document.getElementById('prevButton').addEventListener('click', prePage, false)
-document.getElementById('nextButton').addEventListener('click', nextPage, false)
- async function get(){
+let gets = async () => {
+    // let url = `http://api.mediastack.com/v1/news?access_key=70e250225f82fd05fd9a1daf86aa4a2a&languages=en&countries=in&limit=100`;
     let res = await fetch(url)
     let data = await res.json()
-    DaTa = data.data
+  
+    append1(data.data)
 
-    console.log(data.data)
-    // append(data.data)
 }
+gets()
+function append1(data) {
+    let container = document.getElementById('center')
+    data.forEach((el) => {
+        if (el.image === null) {
+            let div = document.createElement('div')
+            let div1 = document.createElement('div')
+            let div2 = document.createElement('div')
+
+            let img = document.createElement('img')
+            img.src = "https://c.ndtvimg.com/ndtv-logo_625x300_1530090600008.jpg"
+
+            let title = document.createElement('h3')
+            title.innerText = el.title;
+            let press = document.createElement('h6')
+            press.innerText = `Press ${el.source}`;
+            let date = document.createElement('h6')
+            date.innerText = ` | ${el.published_at}`;
+            let place = document.createElement('h6')
+            place.innerText = `,${el.country}`;
+            let des = document.createElement('p')
+            des.innerText = el.description
+            div2.append(press, date, place)
+            div2.setAttribute('id', 'press')
+            div.append(title, div2, des)
+            div.setAttribute('id', 'headline')
+            div1.append(img, div)
+            div1.setAttribute('id', 'second')
+            div1.addEventListener('click', function () {
+                Save(el)
+            })
+
+            container.append(div1);
+        } else {
+            let div = document.createElement('div')
+            let div1 = document.createElement('div')
+            let div2 = document.createElement('div')
+
+            let img = document.createElement('img')
+            img.src = el.image;
+
+            let title = document.createElement('h3')
+            title.innerText = el.title;
+            let press = document.createElement('h6')
+            press.innerText = `Press ${el.source}`;
+            let date = document.createElement('h6')
+            date.innerText = ` | ${el.published_at}`;
+            let place = document.createElement('h6')
+            place.innerText = `,${el.country}`;
+            let des = document.createElement('p')
+            des.innerText = el.description
+            div2.append(press, date, place)
+            div2.setAttribute('id', 'press')
+            div.append(title, div2, des)
+            div.setAttribute('id', 'headline')
+            div1.append(img, div)
+            div1.setAttribute('id', 'second')
+            div1.addEventListener('click', function () {
+                Save(el)
+            })
+
+            container.append(div1); 
+        }
+    });
+}
+ 
 function Save(el) {
     localStorage.setItem('news', JSON.stringify(el));
     window.location.href = "india/news_detail.html";
@@ -82,7 +87,7 @@ function dave(el) {
 }
 
 
-let gets = async () => {
+let get = async () => {
     let url = `https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=cac2a3600da541eebf8893025a6f052a`;
     let res = await fetch(url)
     let data = await res.json()
@@ -91,28 +96,47 @@ let gets = async () => {
 
 
 }
-gets()
+get()
 
 function append(data) {
     let container1 = document.getElementById('top_stories')
     data.forEach((el) => {
-        let div = document.createElement('div')
-        let div1 = document.createElement('div')
+        if (el.urlToImage === null) {
+            let div = document.createElement('div')
+            let div1 = document.createElement('div')
 
-        let img = document.createElement('img')
-        img.src = el.urlToImage
+            let img = document.createElement('img')
+            img.src = "https://c.ndtvimg.com/ndtv-logo_625x300_1530090600008.jpg"
 
-        let title = document.createElement('h5')
-        title.innerText = el.title;
-        let hr = document.createElement('hr')
+            let title = document.createElement('h5')
+            title.innerText = el.title;
+            let hr = document.createElement('hr')
 
-        div.append(title)
-        div1.append(img, div)
-        div1.setAttribute('id', 'first')
-        div1.addEventListener('click', function () {
-            dave(el)
-        })
-        container1.append(div1, hr)
+            div.append(title)
+            div1.append(img, div)
+            div1.setAttribute('id', 'first')
+            div1.addEventListener('click', function () {
+                dave(el)
+            })
+            container1.append(div1, hr)
+        } else {
+            let div = document.createElement('div')
+            let div1 = document.createElement('div')
 
+            let img = document.createElement('img')
+            img.src = el.urlToImage
+
+            let title = document.createElement('h5')
+            title.innerText = el.title;
+            let hr = document.createElement('hr')
+
+            div.append(title)
+            div1.append(img, div)
+            div1.setAttribute('id', 'first')
+            div1.addEventListener('click', function () {
+                dave(el)
+            })
+            container1.append(div1, hr)
+        }
     });
 }
